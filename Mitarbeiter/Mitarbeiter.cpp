27ddb36b846d2315796b86268c5be7e9b8930032
salhfe <salhfeplar@gmail.com>
2024-12-15 16:08:05 +0300

    Mitarbeiter.cpp

--- Mitarbeiter/Mitarbeiter.cpp
+++ Mitarbeiter/Mitarbeiter.cpp
@@ -1,74 +1,82 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include "Worker.h"
+#include "WorkerDataBase.h"
 
 using namespace std;
 
-class worker
+void cinWorker(WorkerDatabase* w)
 {
-private:
-    string name;
-    string familia;
-    int age;
-public:
-    worker() : name(""), familia(""), age(0) {}  // Использую инициализацию членов в конструкторе
-
-    ~worker() {}
-
-        void getName()
-        {
-            cin >> name;
-        }
-
-        void getFamilia()
-        {
-            cin >> familia;
-        }
-
-        void getAge()
-        {
-            cin >> age;
-        }
-
-        // Перегружаю операторы
-        const string& getName() const { return name; }
-
-        const string& getFamilia() const { return familia; }
-
-        int getAge() const { return age; }
-};
-
-class workerDatabase
-{
-private:
-    vector<worker> workers;
-public:
-    void addWorker()
-    {
-        worker newWorker;
-        cout << "\nНапишите имя работника: ";
-        newWorker.getName();
-        cout << "\nНапишите фамилию работника: ";
-        newWorker.getFamilia();
-        cout << "\nНапишите возраст работника: ";
-        newWorker.getAge();
-
-        workers.push_back(newWorker);
-    }
-
-    void displayWorkers() const {
-            for (const auto &localWorker : workers) 
-            {
-                cout << localWorker << endl; // ошибка отсутствует оператор "<<", соответствующий этим операндам
-
-            }
-    }
-};
+    string nameS, familiaS;
+    cout << "\nВведите имя сотрудника: ";
+    cin >> nameS;
+    cout << "\nВведите фамилию сотрудника: ";
+    cin >> familiaS;
+    w->removeWorker(nameS, familiaS);
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    workerDatabase w;
-    w.addWorker();
-    w.displayWorkers();
+
+    bool choice;
+
+    int ch;
+
+    WorkerDatabase w;
+
+    cout << "1: Создать сотрудника\n";
+    cout << "2: Удалить сотрудника\n";
+    cout << "3: Показать список сотрудников\n";
+    cout << "4: Экспорт базы данных\n";
+    cout << "5: Импорт базы данных\n";
+
+    do
+    {
+        x:
+        cout << "\n\nВыберите действие: ";
+        cin >> ch;
+        if (ch == 1)
+        {
+            w.addWorker();
+        }
+        else if(ch == 2)
+        {
+            cinWorker(&w);
+        }
+        else if (ch == 3)
+        {
+            w.displayWorkers();
+        }
+        else if (ch == 4)
+        {
+            w.saveToFile("Idk.txt");
+            cout << "\nБаза экспортирована!";
+        }
+        else if (ch == 5)
+        {
+            w.loadFromFile("Idk.txt");
+            cout << "\nБаза импортированна!";
+        }
+        else
+        {
+            cout << "\nОшибка!\n";
+            goto x;
+        }
+
+        cout << "\n\nПродолжим? Если да, напишите цифру 1\nЕсли нет, напишите любую другую цифру: ";
+        cin >> ch;
+        if (ch == 1)
+        {
+            choice = true;
+        }
+        else
+        {
+            return 0;
+        }
+    } while (choice);
+    return 0;
 }
